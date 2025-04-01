f0744a371f902854857782d54fc60a513e9858d8
Rob <Rob@MacBook-Pro-3.local>
2025-03-31 21:57:16 -0400

    Added cookie based authentication in App routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Suspense, useEffect } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import { CSpinner, useColorModes } from '@coreui/react'
 import './scss/style.scss'
+
+/* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
@@ -13,6 +15,18 @@ const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+
+// Mock authentication function
+const isAuthenticated = () => {
+  // Replace this with your actual authentication logic
+  // return !!localStorage.getItem('token')
+  return true
+}
+
+// Protected route component
+const PrivateRoute = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="login" />
+}
 
 const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
@@ -46,7 +60,15 @@ const App = () => {
           <Route exact path="/register" name="Register Page" element={<Register />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
-          <Route path="*" name="Home" element={<DefaultLayout />} />
+          <Route
+            path="*"
+            name="Home"
+            element={
+              <PrivateRoute>
+                <DefaultLayout />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </Suspense>
     </HashRouter>
