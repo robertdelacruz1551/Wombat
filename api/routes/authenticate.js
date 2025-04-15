3686ddab8f211f19a428f89a70ea2e4d5dac2a58
Robert De La Cruz <snailinslowmotion@gmail.com>
2025-04-15 19:35:36 -0400

    added authentication functions

--- api/routes/authenticate.js
+++ api/routes/authenticate.js
@@ -1,41 +1,48 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
 var router = express.Router();
 
 router.post('/login', async function(req, res) {
   const { email, password } = req.body;
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) {
-    return res.status(401).json({ error: "Invalid credentials" });
+  if (email && password) {
+    const user = await req.app.locals.db.user.findUnique({ where: { email: email } });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const authentic = await bcrypt.compare(password, user.password);
+    if (!authentic) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const payload = { userId: user.id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15m" });
+    res.cookie("token", token, { httpOnly: true, maxAge: 15 * 60 * 1000});
+
+    // ensure that the password is not sent to the client
+    const userData = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      token: token,
+    };
+    res.json(userData);
+  } else {
+    return res.status(401).json({ error: 'Missing credentials' });
   }
-
-  const authentic = await bcrypt.compare(password, user.password);
-  if (!authentic) {
-    return res.status(401).json({ error: 'Invalid credentials' });
-  }
-
-  const payload = { userId: user.id };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15m" });
-  res.cookie("token", token, { httpOnly: true, maxAge: 15 * 60 * 1000});
-
-  // ensure that the password is not sent to the client
-  const userData = {
-    id: user.id,
-    email: user.email,
-    name: user.name,
-  };
-
-  res.json(userData);
 });
 
 router.post('/register', async function(req, res) {
   const { email, password, name } = req.body;
-  const exist = await prisma.user.findUnique({ where: { email } });
+  const exist = await req.app.locals.db.user.findUnique({ where: { email } });
   if (exist) {
     return res.status(400).json({ error: 'User already exists' });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await prisma.user.create({
+  const newUser = await req.app.locals.db.user.create({
     data: { email, password: hashedPassword, name },
     select: { id: true, email: true, name: true },
   });
